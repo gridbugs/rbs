17ad87fdf31215e9eb5fe019c021f958555113d8
Webmin Head <webmin.head@cserevue.org.au>
2012-09-09 01:14:13 +1000

    Fixed a bug in admin booking list.

--- admin_bookinglist.php
+++ admin_bookinglist.php
@@ -14,7 +14,8 @@ include_once('includes/bookingmanagement.php');
 include_once('includes/frames/priceselection.php');
 
 if(isset($_GET['production']) && production_exists($link, $_GET['production']))
-	$_SESSION['admin_production'] = $_GET['admin_production'];
+	$_SESSION['admin_production'] = $_GET['production'];
+
 
 if(!isset($_SESSION['admin_production'])) {
 	echo("production does not exist");
