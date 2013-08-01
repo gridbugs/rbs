f28f2bf49bb7850fefe4f4f8143f31f62c78d456
Christopher Manouvrier <cmanou@mac.com>
2013-08-01 11:58:27 +1000

    Bug Fixes ON prodlist

--- admin_prodlist.php
+++ admin_prodlist.php
@@ -29,22 +29,22 @@ $prodlist = get_admin_prodlist($link);
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
     <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1">
     <title>RBS Admin - Production List</title>
-    <link rel="apple-touch-icon" href="../images/apple-icons/apple-touch-icon-precomposed.png">
-    <link rel="apple-touch-icon" sizes="57x57" href="../images/apple-icons/apple-touch-icon-57x57-precomposed.png">
-    <link rel="apple-touch-icon" sizes="72x72" href="../images/apple-icons/apple-touch-icon-72x72-precomposed.png">
-    <link rel="apple-touch-icon" sizes="114x114" href="../images/apple-icons/apple-touch-icon-114x114-precomposed.png">
-    <link rel="apple-touch-icon" sizes="144x144" href="../images/apple-icons/apple-touch-icon-144x144-precomposed.png">
-    <meta name="msapplication-TileImage" content="../images/apple-icons/apple-touch-icon-144x144-precomposed.png">
+    <link rel="apple-touch-icon" href="images/apple-icons/apple-touch-icon-precomposed.png">
+    <link rel="apple-touch-icon" sizes="57x57" href="images/apple-icons/apple-touch-icon-57x57-precomposed.png">
+    <link rel="apple-touch-icon" sizes="72x72" href="images/apple-icons/apple-touch-icon-72x72-precomposed.png">
+    <link rel="apple-touch-icon" sizes="114x114" href="images/apple-icons/apple-touch-icon-114x114-precomposed.png">
+    <link rel="apple-touch-icon" sizes="144x144" href="images/apple-icons/apple-touch-icon-144x144-precomposed.png">
+    <meta name="msapplication-TileImage" content="images/apple-icons/apple-touch-icon-144x144-precomposed.png">
     <meta name="msapplication-TileColor" content="#ffffff">
     <!-- Modernizr -->
-    <script src="../js/libs/modernizr-2.6.2.min.js"></script>
+    <script src="js/libs/modernizr-2.6.2.min.js"></script>
     <!-- jQuery -->
-    <script type="text/javascript" src="../js/libs/jquery-1.8.2.min.js"></script>
+    <script type="text/javascript" src="js/libs/jquery-1.8.2.min.js"></script>
     <!-- framework css -->
-    <link type="text/css" rel="stylesheet" href="../css/groundwork.css"><!--[if IE]>
-    <link type="text/css" rel="stylesheet" href="../css/groundwork-ie.css"><![endif]--><!--[if lt IE 9]>
-    <script type="text/javascript" src="../js/libs/html5shiv.min.js"></script><![endif]--><!--[if IE 7]>
-    <link type="text/css" rel="stylesheet" href="../css/font-awesome-ie7.min.css"><![endif]-->
+    <link type="text/css" rel="stylesheet" href="css/groundwork.css"><!--[if IE]>
+    <link type="text/css" rel="stylesheet" href="css/groundwork-ie.css"><![endif]--><!--[if lt IE 9]>
+    <script type="text/javascript" src="js/libs/html5shiv.min.js"></script><![endif]--><!--[if IE 7]>
+    <link type="text/css" rel="stylesheet" href="css/font-awesome-ie7.min.css"><![endif]-->
     <script type="text/javascript">
       // extend Modernizr to have datauri test
       (function(){
@@ -56,7 +56,7 @@ $prodlist = get_admin_prodlist($link);
           Modernizr.addTest('datauri', function () { return (datauri.width == 1 && datauri.height == 1); });
           Modernizr.load({
             test: Modernizr.datauri,
-            nope: '../css/no-datauri.css'
+            nope: 'css/no-datauri.css'
           });
         };
         datauri.src = "data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///ywAAAAAAQABAAACAUwAOw==";
@@ -65,15 +65,15 @@ $prodlist = get_admin_prodlist($link);
       Modernizr.load({
         test: Modernizr.inlinesvg,
         nope: [
-          '../css/no-svg.css'
+          'css/no-svg.css'
         ]
       });
       // polyfill for HTML5 placeholders
       Modernizr.load({
         test: Modernizr.input.placeholder,
         nope: [
-          '../css/placeholder_polyfill.css',
-          '../js/libs/placeholder_polyfill.jquery.js'
+          'css/placeholder_polyfill.css',
+          'js/libs/placeholder_polyfill.jquery.js'
         ]
       });
       
@@ -103,7 +103,7 @@ $prodlist = get_admin_prodlist($link);
 
 <div class="container">
       <article class="row">
-        <section class="four fourths left-one padded"><img src="http://placehold.it/300x200/198d98/ffffff/" alt="" class="pull-left gap-right gap-bottom">
+        <section class="padded">
           <h1>Production List</h1>
 			<?
 			foreach($prodlist as $prod) {
@@ -127,8 +127,8 @@ $prodlist = get_admin_prodlist($link);
       <p>&copy 2013 CSE Revue</p>
     </footer>
     <!-- scripts-->
-    <script type="text/javascript" src="../js/plugins/jquery.cycle2.js"></script>
-    <script type="text/javascript" src="../js/groundwork.all.js"></script>
+    <script type="text/javascript" src="js/plugins/jquery.cycle2.js"></script>
+    <script type="text/javascript" src="js/groundwork.all.js"></script>
     <!-- google analytics-->
     <script type="text/javascript">
       var _gaq = _gaq || [];
