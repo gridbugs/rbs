33744dc5e817b2ec31f82e112a431e27a4fee6be
Christopher Manouvrier <cmanou@mac.com>
2013-08-01 14:42:57 +1000

    Fixing login

--- admin_login.php
+++ admin_login.php
@@ -8,15 +8,35 @@ include_once('includes/prodmanagement.php');
 $link = db_connect();
 
 ?>
-<html>
-<head><title>RBS Administration Section</title></head>
+
+<!DOCTYPE html>
+<!--[if lt IE 7]><html class="no-js lt-ie9 lt-ie8 lt-ie7"><![endif]-->
+<!--[if IE 7]>   <html class="no-js lt-ie9 lt-ie8"><![endif]-->
+<!--[if IE 8]>   <html class="no-js lt-ie9"><![endif]-->
+<!--[if gt IE 8]><!-->
+<html lang="en" class="no-js">
+  <!--<![endif]-->
+ <head>
+    <title>RBS Admin></title>
+    <?php include ('includes/groundwork-header.php') ?>  
+ </head>
 <body>
-<h1>RBS Admin Login Page</h1>
-<h2>Login</h2>
-<form method="post" action="admin_logintest.php">
-<p>Email Address or Username: <input type="text" name="email"></p>
-<p>Password: <input type="password" name="pass"></p>
-<input type="submit">
-</form>
+
+<div class="container">
+      <article class="row">
+        <section class="padded">
+			<h1>RBS Admin Login Page</h1>
+			<h2>Login</h2>
+			<form method="post" action="admin_logintest.php">
+			<p>Email Address or Username: <input type="text" name="email"></p>
+			<p>Password: <input type="password" name="pass"></p>
+			<input type="submit">
+			</form>
+ 		</section>
+      </article>
+    </div>
+
+<?php include('includes/page-footer.php') ?>
+
 </body>
 </html>
