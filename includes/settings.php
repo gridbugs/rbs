57a16b2a3ba2fb92650cdc92baeb1a4ec7409202
Stephen Sherratt <steve@stevebob.net>
2014-03-19 10:33:03 +1100

    added dummy settings file

---
+++ includes/settings.php
@@ -0,0 +1,11 @@
+<?
+define('DB_HOSTNAME', 'CHANGE_TO_DB_HOSTNAME');
+define('DB_USERNAME', 'CHANGE_TO_DB_USERNAME');
+define('DB_PASSWORD', 'CHANGE_TO_DB_PASSWORD');
+define('DB_DATABASE', 'CHANGE_TO_DB_NAME');
+
+define('DEBUG', false);
+define('EMAIL_LOG', false);
+
+date_default_timezone_set("Australia/Sydney");
+?>
