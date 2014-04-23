0d74191300c77f086125fe23b1031aa8806b233b
Tracey <chrasy@gmail.com>
2014-04-24 02:41:01 +1000

    updated html for easier styling
    closed self closing tags
    added paragraph tags
    changed heading tags to reflect proper hierarchy

--- login.php
+++ login.php
@@ -31,11 +31,12 @@ print_prod_header($link, $production, $htmlheaders);
 
 <div id="loginblurb">
 
-<align="center"><b>24-27 September 2013</br>
-7:30-10:00pm</br>
-Science Theatre, UNSW</b></align>
+<h1>Production Title</h1>
+<p>24-27 September 2013<br />
+7:30-10:00pm<br />
+Science Theatre, UNSW</p>
 
-<p>You're one step away from getting tickets from one of UNSW's largest and most hilarious productions, the sketch comedy extravaganza with marvelous acting, singing and dancing that's cheaper than a movie. Take a few seconds to make an account and book away!</a>
+<p>You're one step away from getting tickets from one of UNSW's largest and most hilarious productions, the sketch comedy extravaganza with marvellous acting, singing and dancing, and it's cheaper than a movie. Take a few seconds to make an account and book away!</a>
 </p>
 <!-- <p>For the group discount to $10 per ticket (10 or more people), please contact <a href="mailto:producers@cserevue.org.au">producers@cserevue.org.au</a></p> -->
 <p>Having trouble with the ticketing system?
@@ -54,7 +55,7 @@ Please note, <strong>group bookings (<?=($max_booked_seats+1)?> or more seats)</
 
 <div id="login">
 
-<h1>Login</h1>
+<h2>Login</h2>
 <form method="post" action="logintest.php#main">
 <p class="loginblurb">If you have an existing booking please login below.</p>
 <div class="loginfield"><div class="loginlabel">Email Address:</div><input type="text" name="email"></div>
@@ -64,24 +65,24 @@ Please note, <strong>group bookings (<?=($max_booked_seats+1)?> or more seats)</
 </form>
 </div>
 <div id="register">
-<h1>Register</h1>
+<h2>Register</h2>
 <p class="loginblurb">Please enter in your contact details.</p>
 <form method="post" action="register.php#main">
-<div class="loginfield"><div class="loginlabel">Email Address:</div><input type="text" name="email" autofocus="autofocus"></div>
-<div class="loginfield"><div class="loginlabel">Password:</div><input type="password" name="pass"></div>
-<div class="loginfield"><div class="loginlabel">Repeat Password:</div><input type="password" name="pass_repeat"></div>
-<div class="loginfield"><div class="loginlabel">First Name:</div><input type="text" name="fname"></div>
-<div class="loginfield"><div class="loginlabel">Last Name:</div><input type="text" name="lname"></div>
-<div class="loginfield"><div class="loginlabel">Phone Number:</div><input type="text" name="phone"></div>
-<input type="hidden" name="production" value="<?=$prodid?>">
-<div class="loginsubmit"><input type="submit" value="Register"></div>
+<div class="loginfield"><div class="loginlabel">Email Address:</div><input type="text" name="email" autofocus="autofocus" /></div>
+<div class="loginfield"><div class="loginlabel">Password:</div><input type="password" name="pass" /></div>
+<div class="loginfield"><div class="loginlabel">Repeat Password:</div><input type="password" name="pass_repeat" /></div>
+<div class="loginfield"><div class="loginlabel">First Name:</div><input type="text" name="fname" /></div>
+<div class="loginfield"><div class="loginlabel">Last Name:</div><input type="text" name="lname" /></div>
+<div class="loginfield"><div class="loginlabel">Phone Number:</div><input type="text" name="phone" /></div>
+<input type="hidden" name="production" value="<?=$prodid?>" />
+<div class="loginsubmit"><input type="submit" value="Register" /></div>
 </form>
-<div class="loginblurb disclaimer">Your contact details will not be used for any purposes other than the following:
+<div class="loginblurb disclaimer"><p>Your contact details will not be used for any purposes other than the following:</p>
 <ul>
 <li>To send you your booking details and notify you of any necessary changes to your booking.</li>
 <li>To confirm your arrival in case you are running late.</li>
 </ul>
-Your details will not be used for distribution of spam or for marketing purposes and will never be shared with any external entities without your prior permission.</div>
+<p>Your details will not be used for distribution of spam or for marketing purposes and will never be shared with any external entities without your prior permission.</p></div>
 </div>
 
 </div>
