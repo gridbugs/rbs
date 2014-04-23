4648f80afddf71c33ab0fbd2faf9e6cfd33e0296
Tracey Ho <chrasy@gmail.com>
2014-04-24 03:02:12 +1000

    added stylesheet links to the general header

--- includes/frames/prodtheme.php
+++ includes/frames/prodtheme.php
@@ -6,13 +6,18 @@
 function print_prod_header($link, $production, $htmlheaders = "", $bodyattrs = "") {
 ?>
 
-<html>
+<!DOCTYPE html>
 <head>
 <title><?=$production['name']?></title>
 <link href='http://fonts.googleapis.com/css?family=Muli:300,400' rel='stylesheet' type='text/css'>
 <link href='http://fonts.googleapis.com/css?family=Bitter:700' rel='stylesheet' type='text/css'>
 <link rel="stylesheet" type="text/css" href="css/global.css" />
 <link rel="shortcut icon" type="image/ico" href="<?=$production['sitelocation']?>/favicon.ico" />
+<!-- framework css -->
+    <link type="text/css" rel="stylesheet" href="css/groundwork.css"><!--[if IE]>
+    <link type="text/css" rel="stylesheet" href="css/groundwork-ie.css"><![endif]--><!--[if lt IE 9]>
+    <script type="text/javascript" src="js/libs/html5shiv.min.js"></script><![endif]--><!--[if IE 7]>
+    <link type="text/css" rel="stylesheet" href="css/font-awesome-ie7.min.css"><![endif]-->
 <script type="text/javascript" src="js/global.js" ></script>
 
 
