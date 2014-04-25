206c5767a4519b20216811baf591f787a58159eb
Stephen Sherratt <=>
2014-04-25 21:04:56 +1000

    fixed errors when there is no current production

--- includes/page-header.php
+++ includes/page-header.php
@@ -1,5 +1,9 @@
 <header>
+    <? if (isset($production['name'])): ?>
         <h1>RBS Admin - <?=$production['name']?></h1>
+    <? else: ?>
+        <h1>RBS Admin</h1>
+    <?endif?>
 		<div class="row">
         <div class="one fifth">Logged in as: <?=$_SESSION['admin_name']?> (<?=$_SESSION['admin_email']?>)</div>
 
@@ -7,7 +11,7 @@
 <a role="button" href="admin_booking.php">New Booking</a>
 <a role="button" href="admin_bookinglist.php">Booking List</a>
 <a role="button" href="admin_editproduction.php">Edit Production Details</a>
-<?if(can_manage_production($production['id'])):?>
+<?if(isset($production['id']) && can_manage_production($production['id'])):?>
 <a role="button" href="admin_manageticketers.php?prod=<?=$production['id']?>">Manage Ticketers</a>
 <?endif?>
 <a role="button" href="logout.php">Logout</a>
