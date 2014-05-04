f8efe7c1fc5f7adf9a7987b4062dfd9234115efd
Tracey Ho <chrasy@gmail.com>
2014-05-05 01:27:17 +1000

    fixed the messages in the emails sent after booking

--- includes/prodmanagement.php
+++ includes/prodmanagement.php
@@ -129,12 +129,9 @@ function send_confirmation_email_bookingid($link, $booking){
 
 
     $name = $booking['name'];
-    $message = "<img src=rbs.cserevue.org.au/show_data/2013_cse/email_header.png /><p>";
-    $message .= "Dear $name,<p>";
-    $message .= "Thank you for buying tickets to $result[prod], one of UNSW's largest student-run productions that involves sketch comedy, singing and dancing.<br>"; 
-    $message .= "<b>Venue:</b> Science Theatre<br>";
-    $message .= "Anzac Parade<br>";
-    $message .= "Kensington NSW 2052<br>";
+    $message = "Dear $name,<p>";
+    $message .= "Thank you for buying tickets to $result[prod]!<br />"; 
+    $message .= "<b>Venue:</b> UNSW Science Theatre<br />";
     $message .= "<b>Time:</b> Doors open at 7:30pm for a 8pm start.<p>";
     $date = "";
 
@@ -148,22 +145,21 @@ function send_confirmation_email_bookingid($link, $booking){
             $message .= "<ul>";
             $date = $seat['date'];
         }
-        $message .= "<li><b>Ticket for seat $seat[seat]:</b> http://rbs.cserevue.org.au/eticket.php?ticket_id=$seat[guid]$seat[seat] </li>";
+        $message .= "<li><b>Ticket for seat $seat[seat]:</b> http://rbs.cserevue.org.au/eticket.php?ticket_id=$seat[guid]$seat[seat]</li>";
     }
     $message .= "</ul><hr/>";
 
     $message .= "<b>Ticket Info:</b><br>";
     $message .= "<ul>";
     $message .= "<li>The show will start at 8pm sharp. Doors will close at 8:15pm, after which you can only enter during intermission.</li>";
-    $message .= "<li>Please print out each of the tickets found at the links above, or display the link on your phone or other device.</li>"; 
-    $message .= "<li>Group booking ticket holders that enter simultaneously need only present one ticket to validate all seats within the group booking.</li>";
-    $message .= "<li>Group members arriving separately must present each ticket individually.</li>";
-    $message .= "<li>You can email the above seat links to other members of the group booking to facilitate the admissions process</li>";
-    $message .= "<li>Any questions, concerns or issues regarding your tickets can be directed to producers@cserevue.org.au</li>";
+    $message .= "<li>Please print out each of the tickets found at the links above, or display the link on your phone or other device. Paypal receipts are not substitutes for tickets.</li>"; 
+    $message .= "<li>Members of a group booking must present each ticket individually.</li>";
+    $message .= "<li>You can email the above seat links to other members of the group booking to facilitate the admissions process.</li>";
+    $message .= "<li>Any questions, concerns or issues regarding your tickets can be directed to ticketing@cserevue.org.au.</li>";
     $message .= "</ul>";
 
     $message .= "<b> Getting there on foot</b><br>";
-    $message .= "Enter through Anzac Parade Gate and follow the University Mall. Science Theatre is located on the left, opposite the Red Centre.<p>";
+    $message .= "Enter through Anzac Parade Gate and follow the University Mall. The Science Theatre is located on the left, opposite the Red Centre.<p>";
     $message .= "<b> Parking</b><br>";
     $message .= "Enter through Gate 14, Barker St, turn right into Southern Drive.<br>";
     $message .= "Upon exiting vehicle turn right into Engineering Road, turn left into University Mall. Science Theatre is located on the right side, opposite the Red Centre.<p>";
@@ -171,7 +167,7 @@ function send_confirmation_email_bookingid($link, $booking){
     $message .= "Enter through Gate 2, High St, turn left at the Io Myers Theatre, then first right, turn left into Union Rd, then first right. Handicapped patrons may park here in designated bays. Other vehicles should return and exit via Gate 2 and proceed to the Barker St Parking Station.<p>";
 
     $headers = "Content-type: text/html; charset=iso-8859-1 \r\n";
-    $headers .= "From: ticketing@cserevue.org.au\r\n" ;
+    $headers .= "From: ticketing@medrevue.org\r\n" ;
 
     send_email($booking['email'], $result['prod']." eTickets" , $message, $headers);
     print "Emailing $booking[name] ($booking[email])<br/>";
@@ -201,12 +197,9 @@ function send_confirmation_email($link, $seats){
     $seats = sql_get_array($link, $sql);
 
 
-    $message = "<img src=show_data/2013_cse/email_header.png /><p>";
-    $message .= "Dear $result[name],<p>";
-    $message .= "Thank you for buying tickets to $result[prod], one of UNSW's largest student-run productions that involves sketch comedy, singing and dancing.<br>"; 
-    $message .= "<b>Venue:</b> Science Theatre<br>";
-    $message .= "Anzac Parade<br>";
-    $message .= "Kensington NSW 2052<br>";
+    $message = "Dear $result[name],<p>";
+    $message .= "Thank you for buying tickets to $result[prod]!<br />"; 
+    $message .= "<b>Venue:</b>UNSW Science Theatre<br>";
     $message .= "<b>Time:</b> Doors open at 7:30pm for a 8pm start.<p>";
     $date = "";
 
@@ -220,20 +213,18 @@ function send_confirmation_email($link, $seats){
             $message .= "<ul>";
             $date = $seat[date];
         }
-        $message .= "<li><b>Ticket for seat $seat[seat]:</b> http://rbs.cserevue.org.au/eticket.php?ticket_id=$seat[guid]$seat[seat] </li>";
+        $message .= "<li><b>Ticket for seat $seat[seat]:</b> http://rbs.cserevue.org.au/eticket.php?ticket_id=$seat[guid]$seat[seat]</li>";
     }
     $message .= "</ul><hr/>";
-    $message .= "<b>Raffle:</b><br>";
-    $message .= "You're also in the running to win an iPad Mini by completing the form at bit.ly/lr13raffle<p>";
+
 
     $message .= "<b>Ticket Info:</b><br>";
     $message .= "<ul>";
     $message .= "<li>The show will start at 8pm sharp. Doors will close at 8:15pm, after which you can only enter during intermission.</li>";
-    $message .= "<li>Please print out each of the tickets found at the links above, or display the link on your phone or other device.</li>"; 
-    $message .= "<li>Group booking ticket holders that enter simultaneously need only present one ticket to validate all seats within the group booking.</li>";
-    $message .= "<li>Group members arriving separately must present each ticket individually.</li>";
-    $message .= "<li>You can email the above seat links to other members of the group booking to facilitate the admissions process</li>";
-    $message .= "<li>Any questions, concerns or issues regarding your tickets can be directed to producers@cserevue.org.au</li>";
+    $message .= "<li>Please print out each of the tickets found at the links above, or display the link on your phone or other device. Paypal receipts are not substitutes for tickets.</li>"; 
+    $message .= "<li>Members of a group booking must present each ticket individually.</li>";
+    $message .= "<li>You can email the above seat links to other members of the group booking to facilitate the admissions process.</li>";
+    $message .= "<li>Any questions, concerns or issues regarding your tickets can be directed to ticketing@cserevue.org.au.</li>";
     $message .= "</ul>";
 
     $message .= "<b> Getting there on foot</b><br>";
@@ -245,7 +236,7 @@ function send_confirmation_email($link, $seats){
     $message .= "Enter through Gate 2, High St, turn left at the Io Myers Theatre, then first right, turn left into Union Rd, then first right. Handicapped patrons may park here in designated bays. Other vehicles should return and exit via Gate 2 and proceed to the Barker St Parking Station.<p>";
 
     $headers = "Content-type: text/html; charset=iso-8859-1 \r\n";
-    $headers .= "From: tickets@cserevue.org.au\r\n" ;
+    $headers .= "From: ticketing@medrevue.org\r\n" ;
 
     send_email($result[email], $result[prod]." eTickets" , $message, $headers);
     print $message;
